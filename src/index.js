fbc1c7f8c562f7626153cf3a3bbf8d53e5f87863
jean <jin.ahn.1027@gmail.com>
2021-05-04 14:37:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasks=document.getElementById('tasks');
+  const form=document.getElementById('create-task-form');
+
+  form.addEventListener("submit", function(event){
+    const input=document.querySelector("#new-task-description").value;
+    tasks.innerHTML+= `<li> ${input} <button data-action = "delete">x</button> </li>`;
+    event.preventDefault();
+  });
+
+  tasks.addEventListener("click", function(evernt){
+    if (event.target.dataset.action==="delete") {
+      event.target.parentElement.remove();
+    }
+  });
 });
